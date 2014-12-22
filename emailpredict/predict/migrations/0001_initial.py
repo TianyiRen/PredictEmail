58702856852dd3ff1f6f2c7ed1d0c83f8c2a81d7
tr2400 <tr2400@columbia.edu>
2014-12-22 12:13:44 -0500

    update database migration files

--- emailpredict/predict/migrations/0001_initial.py
+++ emailpredict/predict/migrations/0001_initial.py
@@ -13,9 +13,10 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('first_name', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
             ('last_name', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+            ('domain', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
             ('email', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
             ('pattern', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['predict.Pattern'], null=True, blank=True)),
-            ('probability', self.gf('django.db.models.fields.FloatField')(default=1.0)),
+            ('verified', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal(u'predict', ['EmailAddress'])
 
@@ -24,6 +25,7 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('domain', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
             ('pattern', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
+            ('probability', self.gf('django.db.models.fields.FloatField')(default=1.0)),
         ))
         db.send_create_signal(u'predict', ['Pattern'])
 
@@ -39,18 +41,20 @@ class Migration(SchemaMigration):
     models = {
         u'predict.emailaddress': {
             'Meta': {'object_name': 'EmailAddress'},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'email': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['predict.Pattern']", 'null': 'True', 'blank': 'True'}),
-            'probability': ('django.db.models.fields.FloatField', [], {'default': '1.0'})
+            'verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
         },
         u'predict.pattern': {
             'Meta': {'object_name': 'Pattern'},
             'domain': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'pattern': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+            'pattern': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'probability': ('django.db.models.fields.FloatField', [], {'default': '1.0'})
         }
     }
 
